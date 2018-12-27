952d926302258e52f3126ead58871d989d912922
RNO <arnaud.juracek@gmail.com>
2018-12-27 20:20:14 +0100

    Improve panel index fields UX
    + Allow position to be set in situ
    + Set projects subpages numbering to alphabetical, so that user do
    not have to set an irrelevant sorting number
    + Add preview button
    + Add visibility button

--- www/site/plugins/helpers/helpers.panel.php
+++ www/site/plugins/helpers/helpers.panel.php
@@ -8,13 +8,27 @@ class PanelHelpers {
     foreach ($pages as &$page) {
       $p = page($page['uri']);
 
-      $page['parsedPosition'] = '<p style="text-align:center">' . r($p->num(), $p->num(), '-') . '</p>';
       $page['parsedTitle'] = '<b>' . $p->title()->html() . '</b>';
       $page['parsedYear'] = $p->yearBegin()->value() . r($p->yearEnd()->isNotEmpty(), '-' . $p->yearEnd()->value());
       $page['parsedCategories'] = join(' / ', $p->categories()->split());
 
+      $page['position'] = '
+        <a data-modal style="display:block; text-align:center" title="Changer de position" class="btn btn-with-icon" href="' . self::getPanelURL($p, 'toggle?_redirect=pages/' . $p->parent() . '/edit') . '">
+          ' . r($p->num(), $p->num(), '-') . '
+        </a>';
+
+      $page['preview'] = '
+        <a target="_blank" title="Prévisualiser" class="btn btn-with-icon" href="' . url($p->url()) . '">
+          <i class="icon fa fa-play-circle-o"></i>
+        </a>';
+
+      $page['visible'] = '
+        <a data-modal title="'. r($p->isVisible(), 'Rendre invisible', 'Rendre visible') .'" class="btn btn-with-icon" href="' . self::getPanelURL($p, 'toggle?_redirect=pages/' . $p->parent() . '/edit') . '">
+          <i class="icon fa fa-eye'. r($p->isInvisible(), '-slash') . '"></i>
+        </a>';
+
       $page['trash'] = '
-        <a data-modal="" class="btn btn-with-icon" href="' . self::getPanelURL($p, 'delete?_redirect=pages/' . $p->parent() . '/edit') . '">
+        <a data-modal title="Supprimer" class="btn btn-with-icon" href="' . self::getPanelURL($p, 'delete?_redirect=pages/' . $p->parent() . '/edit') . '">
           <i class="icon fa fa-trash-o"></i>
         </a>';
     }
