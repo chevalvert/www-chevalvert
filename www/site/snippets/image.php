6b03906169bb47b7dcbb819cd9b3e400804f4d1f
RNO <arnaud.juracek@gmail.com>
2019-01-02 18:35:41 +0100

    Improve img handling
    + Fix some layout issues with the zoom component
    + Add loading indicator
    + Prevent layout reflow when lazyloading
    + Wrap images in <figure>

--- www/site/snippets/image.php
+++ www/site/snippets/image.php
@@ -10,6 +10,7 @@
     ? $alt
     : $image->caption();
   $title = isset($title) ? $title : $alt;
+  $class = isset($class) ? $class : null;
   $image = $image->focusCrop($width, $height, compact('quality'));
   $linkable = isset($linkable) ? $linkable : false;
 ?>
@@ -17,30 +18,30 @@
 <?php if ($linkable) : ?>
 <a href="<?= $image->original()->url() ?>" data-zoom="<?= $image->original()->url() ?>">
 <?php endif ?>
+  <figure <?= r($class, "class=\"$class\"") ?>>
+    <img
+      width="<?= $image->width() ?>"
+      height="<?= $image->height() ?>"
+      <?= r($alt, 'alt="'.$alt.'"') ?>
+      <?= r($title, 'title="'.$title.'"') ?>
 
-  <img
-    class="<?= isset($class) ? $class : '' ?>"
-    width="<?= $image->width() ?>"
-    height="<?= $image->height() ?>"
-    <?= r($alt, 'alt="'.$alt.'"') ?>
-    <?= r($title, 'title="'.$title.'"') ?>
+      <?php if (isset($attributes)) {
+        foreach ($attributes as $attribute => $value) {
+          echo $attribute . '="'. $value .'"';
+        }
+      } ?>
 
-    <?php if (isset($attributes)) {
-      foreach ($attributes as $attribute => $value) {
-        echo $attribute . '="'. $value .'"';
-      }
-    } ?>
-
-    <?php if (isset($lazy) && $lazy) {
-      echo 'data-lozad ';
-      echo 'src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" ';
-      echo 'data-src="' . $image->url() . '" ';
-    } else {
-      echo 'src="' . $image->url() . '" ';
-    } ?>
-  />
-  <noscript><img src="<?= $image->url() ?>" <?= r($alt, 'alt="'.$alt.'"') ?> <?= r($title, 'title="'.$title.'"') ?>/></noscript>
-
+      <?php if (isset($lazy) && $lazy) {
+        echo 'data-lozad ';
+        // echo 'src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" ';
+        echo 'src="data:image/svg+xml,%3Csvg xmlns=\'http://www.w3.org/2000/svg\' viewBox=\'0 0 '. $image->width() .' '. $image->height() .'\'%3E%3C/svg%3E" ';
+        echo 'data-src="' . $image->url() . '" ';
+      } else {
+        echo 'src="' . $image->url() . '" ';
+      } ?>
+    />
+    <noscript><img src="<?= $image->url() ?>" <?= r($alt, 'alt="'.$alt.'"') ?> <?= r($title, 'title="'.$title.'"') ?>/></noscript>
+  </figure>
 <?php if ($linkable) : ?>
 </a>
 <?php endif ?>
