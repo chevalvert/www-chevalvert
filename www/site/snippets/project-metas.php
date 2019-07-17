22fa18413f9d4ddd7b91dc998fe037e6fbf8b164
RNO <arnaud.juracek@gmail.com>
2019-07-17 15:07:50 +0200

    Improve taxonomy not found handling

--- www/site/snippets/project-metas.php
+++ www/site/snippets/project-metas.php
@@ -5,7 +5,11 @@
 <?php foreach ($project->metas()->toStructure() as $meta) : ?>
   <?php $label = autoid($meta->meta()) ?>
   <li class="project-metas__meta">
-    <span class="project-metas__meta-label"><?= $label ? $label->$lang() : $meta ?></span>
+
+    <?php if ($label) : ?>
+    <span class="project-metas__meta-label"><?= $label->$lang() ?></span>
+    <?php endif ?>
+
     <span class="project-metas__meta-value">
       <?= r($meta->$lang()->isNotEmpty(), $meta->$lang()->html(), $meta->fr()->html()) ?>
     </span>
