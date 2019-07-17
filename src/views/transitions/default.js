3923175c51dd602f845c711466e1abf5dac29e38
RNO <arnaud.juracek@gmail.com>
2019-07-17 10:39:34 +0200

    Fix barba transition error

--- src/views/transitions/default.js
+++ src/views/transitions/default.js
@@ -1,4 +1,4 @@
 export default (oldContainer, newContainer, lastClickEl, done) => {
-  if (!lastClickEl.hasAttribute('data-lang-switcher')) window.scrollTo(0, 0)
+  if (lastClickEl && !lastClickEl.hasAttribute('data-lang-switcher')) window.scrollTo(0, 0)
   return done()
 }
