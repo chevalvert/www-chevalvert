612550588966260f558b1aa0f934bf9ae811b58e
RNO <arnaud.juracek@gmail.com>
2019-07-17 15:59:11 +0200

    Disable Kirby content locking
    As of Kirby 3.2.2, content locking is still really buggy:
    disabling for now using this plugin

---
+++ www/site/plugins/disable-lock/index.js
@@ -0,0 +1,12 @@
+panel.plugin('passport/tmp-lock', {
+  components: {
+    'k-form-buttons': {
+      extends: 'k-form-buttons',
+      methods: {
+        setLock() {
+          return
+        }
+      }
+    }
+  }
+})
---
+++ www/site/plugins/disable-lock/index.php
@@ -0,0 +1,5 @@
+<?php
+
+// NOTE: As of Kirby 3.2.2, content locking is still really buggy: disabling for now using this plugin
+// SEE: https://github.com/getkirby/kirby/issues/1900#issuecomment-510929097
+Kirby::plugin('chevalvert/tmp-lock', []);
