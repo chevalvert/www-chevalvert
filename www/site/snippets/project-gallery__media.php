b097b2cbf4bd1ba798146ba6d97ee94c1152165e
RNO <arnaud.juracek@gmail.com>
2019-07-22 16:03:44 +0200

    Improve thumbnail quality

--- www/site/snippets/project-gallery__media.php
+++ www/site/snippets/project-gallery__media.php
@@ -1,5 +1,6 @@
 <li data-cols="<?= $cols ?>">
   <?php $project->media($line->$key(), [
+    'quality' => 100,
     'width' => $width = option('project-gallery.layouts.thumbnail_sizes')[$cols]
   ]) ?>
 </li>
