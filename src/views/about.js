e1a0f41555b7e15995bdcd0b7ebd882804b574c5
RNO <arnaud.juracek@gmail.com>
2019-09-10 17:02:20 +0200

    Make sure last about-section take the whole space so that anchor could be triggered

--- src/views/about.js
+++ src/views/about.js
@@ -1,5 +1,9 @@
+import { debounce } from 'tiny-throttle'
+
 import view from 'abstractions/barba-view'
 import tocHighlighter from 'controllers/toc-highlighter'
+import lastOf from 'utils/array-last-of'
+import noop from 'utils/noop'
 
 export default view('about', {
   onEnterCompleted: refs => {
@@ -8,5 +12,38 @@ export default view('about', {
       anchorsScrollContainer: document.querySelector('.about__sections'),
       offset: 20
     })
+
+    // NOTE: this allows the last .about-section to be big enough
+    // to trigger controllers/toc-highlighter on its anchor
+    // When last-child is not merged-with-previous-section, previous section
+    // height does not have to be taken in consideration, which is possible in
+    // pure CSS: SEE components/about-section.scss.
+    const sections = document.querySelectorAll('.about-section')
+    const mergedSection = lastOf(sections)
+    refs.autofitLastMergedSection = mergedSection.classList.contains('is-merged-with-previous-section')
+      ? debounce(() => {
+        // Disable autofitting below a specific breakpoint (SEE sass/devices.scss)
+        if (window.innerWidth < 1120) {
+          mergedSection.style['height'] = 'auto'
+          return
+        }
+
+        const sectionHeight = sections[sections.length - 2].offsetHeight
+        const menuHeight = document.querySelector('.menu').offsetHeight
+        const footerHeight = document.querySelector('.footer').offsetHeight
+        const targetHeight = window.innerHeight - (menuHeight * 2) - footerHeight - sectionHeight + 22
+
+        mergedSection.style['height'] = targetHeight + 'px'
+      }, 50)
+      : noop
+
+    window.addEventListener('load', refs.autofitLastMergedSection)
+    window.addEventListener('resize', refs.autofitLastMergedSection) // TODO: throttle
+    refs.autofitLastMergedSection()
+  },
+
+  onLeave: refs => {
+    window.removeEventListener('load', refs.autofitLastMergedSection)
+    window.removeEventListener('resize', refs.autofitLastMergedSection)
   }
 })
