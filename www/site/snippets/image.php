chevalvert/www-chevalvert

commit 0f8cabfd72d7f0f418381090a3ad5deb0be06a8a

Improve SEO & accessibility

www/site/snippets/image.php
@@ -5,8 +5,8 @@
   $quality = $quality ?? 100;
   $crop = $crop ?? false;
 
-  $alt = isset($alt) ? Escape::html($alt) : Escape::html($image->caption());
-  $title = $title ?? $alt;
+  $alt = isset($alt) ? Escape::html($alt) : tt('alt.schema', ['projectTitle' => $image->parent()->title()]);
+  $title = $title ?? Escape::html($image->caption());
   $class = $class ?? '';
   $attributes = $attributes ?? [];
 

src/sass/fonts.scss
@@ -1,5 +1,6 @@
 @font-face {
   font-family: 'Friction Mono';
+  font-display: auto;
   src: url('../fonts/friction-mono.woff2') format('woff2'),
        url('../fonts/friction-mono.woff') format('woff'),
        url('../fonts/friction-mono.otf') format('opentype');

www/site/languages/en.php
@@ -17,6 +17,8 @@ return [
     'projects-list.type' => 'type',
     'projects-list.date' => 'date',
 
-    'categories.all' => 'all'
+    'categories.all' => 'all',
+
+    'alt.schema' => "{ projectTitle }, Chevalvert"
   ]
 ];

www/site/languages/fr.php
@@ -18,6 +18,7 @@ return [
     'projects-list.date' => 'date',
 
     'categories.all' => 'tout',
-  ]
 
+    'alt.schema' => "{ projectTitle }, Chevalvert"
+  ]
 ];

www/site/plugins/social-kirbytags/index.php
@@ -45,7 +45,7 @@ function createSocialTag ($name, $url) {
     'html' => function ($tag) use ($name, $url) {
       $id = site()->$name();
       $text = $tag->$name() ? $tag->$name() : $name;
-      return "<a href=\"$url/$id\" target=\"_blank\">$text</a>";
+      return "<a href='$url/$id' target='_blank' rel='noopener'>$text</a>";
     }
   ];
 }

www/site/snippets/project-preview.php
@@ -1,5 +1,5 @@
 <section class="project-preview">
-  <a href="<?= $project->url() ?>" class="project-preview__header" style="background-color:<?= $project->color()->value() ?>" data-contrast="<?= Contrast::compute($project->color()->value()) ?>">
+  <a href="<?= $project->url() ?>" aria-label="<?= $project->title() ?>" class="project-preview__header" style="background-color:<?= $project->color()->value() ?>" data-contrast="<?= Contrast::compute($project->color()->value()) ?>">
     <?php if ($show_title ?? true) : ?>
     <div class="project__header-column">
       <h3><?= $project->title()->html() ?></h3>
@@ -18,7 +18,7 @@
     </div>
     <?php endif ?>
   </a>
-  <a href="<?= $project->url() ?>" tabIndex="-1">
+  <a href="<?= $project->url() ?>" tabIndex="-1" role="button" aria-label="<?= $project->title() ?>">
     <div class="project-preview__cover">
       <?= $project->cover($cover_preset ?? 'default') ?>
     </div>

www/site/snippets/vimeo.php
@@ -20,13 +20,13 @@
   style="padding-top:<?= number_format((1 / $ratio) * 100, 2) ?>%"
 >
   <?php if ($allow_fullscreen) : ?>
-  <a class="vimeo__link" title="<?= $title ?>" href="<?= $vimeo->externalUrl() ?>"></a>
+  <a class="vimeo__link" title="<?= Escape::html($title) ?>" href="<?= $vimeo->externalUrl() ?>"></a>
   <?php endif ?>
 
   <iframe
     data-ratio="<?= number_format((1 / $ratio), 4) ?>"
     class="<?= r($autoplay, 'no-gui') ?> <?= $class ?>"
-
+    title="<?= Escape::html($title) ?>"
     width="<?= $width ?>"
     height="<?= $height ?>"
 

commit e39023634617c9bf588104811b1e36a7bb928200

Fix regression introduced in 0f8cabf

www/site/snippets/image.php
@@ -25,7 +25,7 @@
 ?>
 
 <?php if ($allow_fullscreen) : ?>
-<a href="<?= $full->url() ?>" data-caption="<?= $alt ?>" data-fullscreen="<?= $full->url() ?>">
+<a href="<?= $full->url() ?>" data-caption="<?= $title ?>" data-fullscreen="<?= $full->url() ?>">
 <?php endif ?>
 
   <figure <?= r($class, "class=\"$class\"") ?>>